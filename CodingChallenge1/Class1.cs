824240b2cd7414b60da9b4d72b86afb2cdb1efd5
roxfordlc <roxfordlc@gmail.com>
2020-04-27 11:11:31 -0500

    Changing the coding challenge to a class based solution

---
+++ CodingChallenge1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge1
+{
+    class SweetSalty
+    {
+        private int _sweetCount;
+        private int _saltyCount;
+        private int _comboCount;
+
+        public SweetSalty()
+        {
+            _sweetCount = 0;
+            _saltyCount = 0;
+            _comboCount = 0;
+        }
+
+        public string TasteTest(int num)
+        {
+            if (num % 5 == 0 && num % 3 == 0)
+            {
+                _comboCount++;
+                return "Sweet'nSalty";
+            }
+            else if (num % 5 == 0)
+            {
+                _saltyCount++;
+                return "Salty";
+            }
+            else if (num % 3 == 0)
+            {
+                _sweetCount++;
+                return "Sweet";
+            }
+            else
+            {
+                return num.ToString();
+            }
+        }
+
+        public void PrintResults(int end)
+        {
+            int i = 1;
+            while (i <= end)
+            {
+                Console.WriteLine(TasteTest(i));
+                i++;
+            }
+            Console.WriteLine($"There are {_sweetCount} sweet, {_saltyCount} salty, and {_comboCount} Sweet'nSalty.");
+        }
+    }
+}
--- CodingChallenge1/Program.cs
+++ CodingChallenge1/Program.cs
@@ -4,34 +4,13 @@ namespace CodingChallenge1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            int sweetCount = 0;
-            int saltyCount = 0; 
-            int comboCount = 0;
-            for(int i = 0; i <= 100; i++)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("Sweet'nSalty");
-                    comboCount++;
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Salty");
-                    saltyCount++;
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Sweet");
-                    sweetCount++;
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }    
-            }
-            Console.WriteLine($"There are {sweetCount} sweet, {saltyCount} salty, and {comboCount} sweet'nSalty present.");
+            //Create a SweetSalty object
+            SweetSalty list = new SweetSalty();
+            //Call the PrintResults method with 100 as the end integer
+            list.PrintResults(100);
         }
     }
 }
